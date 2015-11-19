5ed5c7ba7d9425508f5334e5a029ce4078041bb9
Sjoerd Mulder <sjoerd@frontendless.nl>
2015-11-19 15:48:45 +0100

    Fix for issue #2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,15 @@ fi
 
 if [ ! -d "$AGENT_DIR/bin" ]; then
     echo "$AGENT_DIR doesn't exist pulling build-agent from server $TEAMCITY_SERVER";
+    let waiting=0
+    until curl -s -f -I -X GET $TEAMCITY_SERVER/update/buildAgent.zip; do
+        let waiting+=3
+        sleep 3
+        if [ $waiting -eq 120 ]; then
+            echo "Teamcity server did not respond within 120 seconds"...
+            exit 42
+        fi
+    done
     wget $TEAMCITY_SERVER/update/buildAgent.zip && unzip -d $AGENT_DIR buildAgent.zip && rm buildAgent.zip
     chmod +x $AGENT_DIR/bin/agent.sh
     echo "serverUrl=${TEAMCITY_SERVER}" > $AGENT_DIR/conf/buildAgent.properties
