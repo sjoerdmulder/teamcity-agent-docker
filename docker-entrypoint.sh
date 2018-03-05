f4d0d2b22983f4036b1207b2c205093a1e1db439
Michael Ruhwedel <mruhwedel@small-improvements.com>
2018-03-05 13:14:36 +0100

    feat(agent) re-configure agents on start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,24 +22,23 @@ mkdir -p $AGENT_DIR/conf
 
 configure(){
     local CONFIG=$AGENT_DIR/conf/buildAgent.properties
-    sed -i 'd/'$'$1''.*/' $CONFIG
+    sed -i'' "/^$1.*/d" $CONFIG
     echo $2 >> $CONFIG
 }
 
 
-configure serverUrl "serverUrl=$TEAMCITY_SERVER"
-configure workDir "workDir=$AGENT_DIR/work"
-configure tempDir "$AGENT_DIR/temp"
-configure name "Agent-$AGENT_NUMBER"
-
 if [ ! "$(ls -A $AGENT_DIR)" ]; then
     echo "$AGENT_DIR is empty, pulling build-agent from server $TEAMCITY_SERVER";
-
     wget $TEAMCITY_SERVER/update/buildAgent.zip &&\
     unzip -d $AGENT_DIR buildAgent.zip &&\
     rm buildAgent.zip
     chmod +x $AGENT_DIR/bin/agent.sh
 fi
+
+configure serverUrl serverUrl=$TEAMCITY_SERVER
+configure workDir workDir=$AGENT_DIR/work
+configure tempDir tempDir=$AGENT_DIR/temp
+configure name name=Agent-$AGENT_NUMBER
 
 if [ ! -z $GRADLE_USER_HOME ]; then
     echo "Using Gradle cache in $GRADLE_USER_HOME"
