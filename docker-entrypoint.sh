5731660e2e2c6c88a2500c647224028fed265cc3
Michael Ruhwedel <mruhwedel@small-improvements.com>
2019-01-09 18:54:03 +0100

    fix(tc-agent) create dir / remove deprecated env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,25 +20,20 @@ configure(){
     echo $2 >> ${CONFIG}
 }
 
+mkdir -p $AGENT_DIR/conf
+
 if [[ ! -f ${CONFIG} ]]; then
     echo "$AGENT_DIR is empty, pulling build-agent from server $TEAMCITY_SERVER";
-    wget $TEAMCITY_SERVER/update/buildAgent.zip &&\
+    wget -q $TEAMCITY_SERVER/update/buildAgent.zip &&\
     unzip -d $AGENT_DIR buildAgent.zip &&\
     rm buildAgent.zip
     chmod +x $AGENT_DIR/bin/agent.sh
 fi
 
-mkdir -p $AGENT_DIR/conf
-
 configure serverUrl serverUrl=$TEAMCITY_SERVER
 configure workDir workDir=$AGENT_DIR/work
 configure tempDir tempDir=$AGENT_DIR/temp
 configure name name=Agent-$AGENT_NUMBER
-
-if [ ! -z $GRADLE_USER_HOME ]; then
-    echo "Using Gradle cache in $GRADLE_USER_HOME"
-    chown -R teamcity-agent:root $GRADLE_USER_HOME
-fi
 
 chown -R teamcity-agent:root $AGENT_DIR
 
